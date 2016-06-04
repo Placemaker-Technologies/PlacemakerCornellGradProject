8fb5f0024c6637aef2f4e200fedebdd62169b5e2
sanhitamukherji <sanhita.mukherji@gmail.com>
2016-06-03 22:50:48 -0400

    Replace the file entirely

--- RESTCode/settings.js
+++ RESTCode/settings.js
@@ -1,11 +1,1 @@
-exports.dbConfig={
- conString :"pg://postgres:postgres@localhost:5432/placemaker"
-};
-exports.URL={
-	bucketName:'edu-cornell-placemaker-images',
-	domain:"s3.amazonaws.com"
-};
-exports.Icon_URL={
-	bucketName:'edu-cornell-placemaker-icons',
-	domain:"s3.amazonaws.com"
-};
+
